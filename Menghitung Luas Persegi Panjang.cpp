f0dc9709a0aa1d914e19afe3e19cfd3cc9d22d87
roni46 <39713502+roni46@users.noreply.github.com>
2019-01-03 17:36:45 +0700

    Add files via upload

---
+++ Menghitung Luas Persegi Panjang.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int p,l,luas;
+printf("PROGRAM MENGHITUNG LUAS PERSEGI PANJANG\n");
+printf("BY M Achmad Sahroni NIM 311620782\n");
+printf("KAMPUS PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+cout<<"masukan panjang:";
+cin>>p;
+cout<<"masukan lebar:";
+cin>>l;
+luas=p*l;
+cout<<"luas persegi panjang adalah "<<luas;
+return 0;
+}
+
